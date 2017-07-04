5daf77e72c95acf9403b417ed53caa28fce9039b
skost <kostornychenko@gmail.com>
2017-07-04 21:07:18 +0300

    homework 3

---
+++ 4-JS/js/main.js
@@ -0,0 +1,111 @@
+'use strict';
+
+//alert("hello from file");
+
+/*
+alert("hello from file");
+*/
+
+//alert("hello from file");
+
+var id = 3;
+var type = "message";
+var $ = 12321;
+//alert($);
+
+var Name;
+var name;
+
+var NameBySort;
+var name_sort_one;
+var CONSTANT = 45;
+//alert(CONSTANT);
+
+//number
+var num = 345435;
+var d = 4324.65;
+
+//string
+var str = "lorem";
+var str1 = 'fdsfdsfdsfdsf';
+var s = 'd';
+
+//boolean
+var r = true;
+var ss = false;
+
+//null
+var y = null;
+
+
+//undefined
+var un;
+
+//object
+var obj = {asd: 'qwerty'};
+
+//alert(typeof (obj));
+
+////////////////
+
+/*var q = -45;
+var str15 = "12s";
+alert(typeof str15);
+
+alert(str15);
+
+alert(typeof +str15);
+alert(+str15);*/
+
+var s1 = "string1";
+var s2 = "string2";
+
+var d1 = 15;
+var d2 = 25;
+
+//alert(d1 * s2);
+//alert(s1 + s2);
+
+var a, b, c;
+a = b = c = 34 + +"15";
+// alert(b);
+// a = 10;
+// alert(b);
+
+a++;
+++a;
+
+b--;
+--b;
+
+// a >= b
+// a <= b
+//
+// a % b
+// a != b
+//
+// a == b
+// a === b
+// a !== b
+
+
+//alert("assss" > "as");
+console.log("assss" > "as");
+
+//var d2 = prompt("enter number", 10);
+var l = confirm("text text");
+alert(typeof l);
+alert(l);
+
+
+
+
+
+
+
+
+
+
+
+
+
